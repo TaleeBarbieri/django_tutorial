eaaa1985b34d61a3059957102ed247954a9dbc95
TaleeBarbieri <talee.alexei@hotmail.com>
2023-05-18 16:49:18 +0100

    view price in product_detail

--- MyWebApp/products_app/views.py
+++ MyWebApp/products_app/views.py
@@ -7,6 +7,7 @@ def product_detail_view(request):
     obj = Product.objects.get(id=1)
     context = {
         'name': obj.name,
-        'description': obj.description
+        'description': obj.description,
+        'price': obj.price
     }
     return render(request, "products_apps/detail.html", context)
